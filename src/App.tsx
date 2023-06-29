3ca7bcb539fd32a44f157a7a3febc7f554f6616f
Sebster <sebster.dev@gmail.com>
2023-06-29 21:27:57 +0100

    feat: allow selection of trading pairs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,93 @@
-import { getTradingPairs } from "../src/api/coinbase.ts";
 import { useState, useEffect } from "react";
 
+import type { PairData } from "../src/api/coinbase.ts";
+import { getTradingPairs } from "../src/api/coinbase.ts";
+
+type Currencies = Record<string, string[]>;
+
+const getCurrenciesFromData = (pairsData: PairData[]): Currencies => {
+	const currencies: Currencies = {};
+
+	pairsData.forEach((pairData) => {
+		const { base_currency, quote_currency } = pairData;
+		currencies[base_currency] = [
+			...(currencies[base_currency] ?? []),
+			quote_currency,
+		];
+	});
+
+	return currencies;
+};
+
+// ----------------------------------------------------------------------------
+
 function App() {
-	const [pairsData, setPairsData] = useState("");
+	const [currencies, setCurrencies] = useState<Currencies>();
+
+	const [baseCurrencies, setBaseCurrencies] = useState([""]);
+	const [quoteCurrencies, setQuoteCurrencies] = useState([""]);
+
+	const [baseCurrencySelected, setBaseCurrencySelected] = useState("");
+	const [quoteCurrencySelected, setQuoteCurrencySelected] = useState("");
 
 	useEffect(() => {
-		getTradingPairs().then(setPairsData);
+		getTradingPairs().then(
+			(data) => data && setCurrencies(getCurrenciesFromData(data))
+		);
 	}, []);
 
-	return <pre>{JSON.stringify(pairsData, null, 2)}</pre>;
+	useEffect(() => {
+		if (!currencies) return;
+		const newBaseCurrencies = Object.keys(currencies);
+		setBaseCurrencies(newBaseCurrencies);
+		setBaseCurrencySelected(newBaseCurrencies[0]);
+	}, [currencies]);
+
+	useEffect(() => {
+		if (!currencies) return;
+		const newQuoteCurrencies = currencies[baseCurrencySelected] ?? [""];
+		setQuoteCurrencies(newQuoteCurrencies);
+	}, [currencies, baseCurrencySelected]);
+
+	useEffect(() => {
+		if (!quoteCurrencies.includes(quoteCurrencySelected)) {
+			setQuoteCurrencySelected(quoteCurrencies[0]);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [quoteCurrencies, baseCurrencySelected]);
+
+	const handleSelectBaseCurrency = (e: React.ChangeEvent<HTMLSelectElement>) =>
+		setBaseCurrencySelected(e.target.value);
+
+	const handleSelectQuoteCurrency = (e: React.ChangeEvent<HTMLSelectElement>) =>
+		setQuoteCurrencySelected(e.target.value);
+
+	return (
+		<>
+			<select
+				name="base currency"
+				value={baseCurrencySelected}
+				onChange={handleSelectBaseCurrency}
+			>
+				{baseCurrencies.map((currency) => (
+					<option key={currency} value={currency}>
+						{currency}
+					</option>
+				))}
+			</select>
+			<select
+				name="quote currency"
+				value={quoteCurrencySelected}
+				onChange={handleSelectQuoteCurrency}
+			>
+				{quoteCurrencies.map((currency) => (
+					<option key={currency} value={currency}>
+						{currency}
+					</option>
+				))}
+			</select>
+		</>
+	);
 }
 
 export default App;
--- src/api/coinbase.ts
+++ src/api/coinbase.ts
@@ -2,10 +2,16 @@ import axios from "axios";
 
 const PRODUCTS_URL = "https://api.exchange.coinbase.com/products";
 
+export type PairData = Record<string, unknown> & {
+	id: string;
+	base_currency: string;
+	quote_currency: string;
+};
+
 export const getTradingPairs = () =>
 	axios
 		.get(PRODUCTS_URL, {
 			headers: { "Content-Type": "application/json" },
 		})
-		.then((res) => res.data)
+		.then<PairData[]>((res) => res.data)
 		.catch(console.error);
